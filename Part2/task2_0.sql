be1527498df8e735422c0de11bbdd4c4a64ea84a
Artem4ikPksh <127250289+Artem4ikPksh@users.noreply.github.com>
2025-09-01 01:12:59 +0300

    Add files via upload

---
+++ Part2/task2_0.sql
@@ -0,0 +1,55 @@
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+SELECT
+    (COUNT(T2.user_id) * 1.0) / COUNT(T1.user_id) AS conversion_rate
+FROM
+    (
+        SELECT
+            user_id
+        FROM
+            product_analyst
+        WHERE
+            product_id = 'tenwords_1w_9.99_offer'
+            AND refunded = 'False'
+        GROUP BY
+            user_id
+    ) AS T1
+LEFT JOIN
+    (
+        SELECT
+            DISTINCT T2_sub.user_id
+        FROM
+            product_analyst AS T2_sub
+        JOIN
+            (
+                SELECT
+                    user_id,
+                    MIN(purchase_date) AS first_purchase_date
+                FROM
+                    product_analyst
+                WHERE
+                    product_id = 'tenwords_1w_9.99_offer'
+                    AND refunded = 'False'
+                GROUP BY
+                    user_id
+            ) AS T1_sub
+        ON T2_sub.user_id = T1_sub.user_id
+        WHERE
+            T2_sub.purchase_date > T1_sub.first_purchase_date
+            AND T2_sub.product_id = 'tenwords_1w_9.99_offer'
+            AND T2_sub.refunded = 'False'
+    ) AS T2
+ON T1.user_id = T2.user_id  COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ Part2/task2_2.sql
@@ -0,0 +1,33 @@
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+SELECT DISTINCT user_id
+FROM (
+    SELECT 
+        t.user_id,
+        t.purchase_date,
+        t.refunded,
+        @row_num := IF(@prev_user = t.user_id, @row_num + 1, 1) AS transaction_number,
+        @prev_user := t.user_id
+    FROM file3 t
+    CROSS JOIN (SELECT @row_num := 0, @prev_user := NULL) vars
+    ORDER BY t.user_id, t.purchase_date
+) ranked
+WHERE (transaction_number = 1 AND refunded = 'FALSE')
+   OR (transaction_number = 2 AND refunded = 'TRUE')
+GROUP BY user_id
+HAVING SUM(transaction_number = 1 AND refunded = 'FALSE') > 0
+   AND SUM(transaction_number = 2 AND refunded = 'TRUE') > 0  COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
